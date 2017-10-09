058ddd2e8931d1595b3ca2ac570bf9428800006b
Raed Atoui <raed.atoui@gmail.com>
2017-10-09 12:46:13 -0400

    move local storage mock into shim

--- src/setup_tests.js
+++ src/setup_tests.js
@@ -3,21 +3,3 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
-
-class LocalStorageMock {
-  store;
-
-  constructor() {
-    this.store = {};
-  }
-
-  getItem(key: string): ?string {
-    return this.store[key] || null;
-  }
-
-  setItem(key: string, value: string): void {
-    this.store[key] = value;
-  }
-}
-
-global.localStorage = new LocalStorageMock();
